76fc5638a2dcb284d5ccce59136340312e116a3e
rishi-bahadoor <122465036+rishi-bahadoor@users.noreply.github.com>
2025-08-05 08:43:21 -0400

    add misc module to simplify repeated code (#8)

--- src/tests.rs
+++ src/tests.rs
@@ -1,30 +1,21 @@
 use crate::commands::*;
 use crate::configure::*;
+use crate::misc::*;
 
 use core::str;
-use std::thread::sleep;
-use std::time::Duration;
 
-use std::io::{self, Write};
-
-pub fn list_sensors() {
-    run_ccc_command("list-sensors");
-}
-
-pub fn test_point_display(args: &str) {
+fn test_point_display(args: &str) {
     configure_default();
     set_pixel_format(args);
     turn_on_points();
 
     println!("Observe the point cloud display.");
-    let timer_2 = 120;
-    println!("Timer: {} seconds", timer_2);
-    for n in 0..timer_2 {
-        sleep(Duration::from_millis(1000));
-        print!("\r{:3} ", timer_2 - n); // Use padding for consistent width
-        io::stdout().flush().unwrap(); // Ensure output is shown immediately
-    }
-    println!(); // Move to a new line after the loop finishes
+    timer_countdown(120);
+    turn_off_points();
+}
+
+pub fn list_sensors() {
+    run_ccc_command("list-sensors");
 }
 
 pub fn test_1_1() {
